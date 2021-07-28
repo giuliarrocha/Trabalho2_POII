247d98afdfc51142498273f23934fdba009c0010
Giulia Rossatto Rocha <giulia.rocha@unesp.br>
2021-07-27 23:59:59 -0300

    Criação da interface e escopo do código

---
+++ po2_trabalho2.py
@@ -0,0 +1,188 @@
+import PySimpleGUI as sg
+from sympy.polys.polyoptions import Symbols
+from py_expression_eval import Expression, Parser
+from sympy import *
+from decimal import Decimal
+import math
+
+sg.theme('LightBrown13')
+
+## Janelas
+def window_hookeJeeves():
+    layout = [[sg.Text('HOOKE & JEEVES', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text('         ')],
+            [sg.Text('Insira a função:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='expressao', size=(30,1))],
+            [sg.Text('Exemplo: x¹ = (0, 0) para R² o ponto inicial',  font=('Arial', 10, 'bold'), text_color = 'black')],
+            [sg.Text('x¹:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='ponto_inicial', size=(10,1))],
+            [sg.Text('ε = ',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='epsilon', size=(10,1))],
+            [sg.Text('         ')],
+            [sg.Button('Calcular', size=(15,1)), sg.Button('Sair', size=(15,1))],
+            [sg.Text('         ')],
+            [sg.Text('         ')],
+            [sg.Text(size=(40,1), key='respostaHooke1',  font=('Arial', 11, 'bold'))],
+            [sg.Text(size=(40,1), key='respostaHooke2', text_color = 'black')],
+            [sg.Text(size=(40,1), key='respostaHooke3', text_color = 'black')]
+            ]
+    return sg.Window('Hooke & Jeeves', layout, size=(400, 400), finalize=True, resizable=True)
+
+def window_gradiente():
+    layout = [[sg.Text('GRADIENTE', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text('         ')],
+            [sg.Text('Insira a função:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='expressao', size=(30,1))],
+            [sg.Text('Exemplo: x¹ = (0, 0) para R² o ponto inicial',  font=('Arial', 10, 'bold'), text_color = 'black')],
+            [sg.Text('x¹:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='ponto_inicial', size=(10,1))],
+            [sg.Text('ε = ',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='epsilon', size=(10,1))],
+            [sg.Text('         ')],
+            [sg.Button('Calcular', size=(15,1)), sg.Button('Sair', size=(15,1))],
+            [sg.Text('         ')],
+            [sg.Text('         ')],
+            [sg.Text(size=(40,1), key='respostaGradiente1',  font=('Arial', 11, 'bold'))],
+            [sg.Text(size=(40,1), key='respostaGradiente2', text_color = 'black')],
+            [sg.Text(size=(40,1), key='respostaGradiente3', text_color = 'black')]
+            ]
+    return sg.Window('Gradiente', layout, size=(400, 400), finalize=True, resizable=True)
+
+def window_fletcherReeves():
+    layout = [[sg.Text('FLETCHER & REEVES', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text('         ')],
+            [sg.Text('Insira a função:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='expressao', size=(30,1))],
+            [sg.Text('Exemplo: x¹ = (0, 0) para R² o ponto inicial',  font=('Arial', 10, 'bold'), text_color = 'black')],
+            [sg.Text('x¹:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='ponto_inicial', size=(10,1))],
+            [sg.Text('ε = ',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='epsilon', size=(10,1))],
+            [sg.Text('         ')],
+            [sg.Button('Calcular', size=(15,1)), sg.Button('Sair', size=(15,1))],
+            [sg.Text('         ')],
+            [sg.Text('         ')],
+            [sg.Text(size=(40,1), key='respostaFletcher1',  font=('Arial', 11, 'bold'))],
+            [sg.Text(size=(40,1), key='respostaFletcher2', text_color = 'black')],
+            [sg.Text(size=(40,1), key='respostaFletcher3', text_color = 'black')]
+            ]
+    return sg.Window('Fletcher & Reeves', layout, size=(400, 400), finalize=True, resizable=True)
+
+def window_davidonFletcherPowell():
+    layout = [[sg.Text('DAVIDON-FLETCHER-POWELL', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text('         ')],
+            [sg.Text('Insira a função:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='expressao', size=(30,1))],
+            [sg.Text('Exemplo: x¹ = (0, 0) para R² o ponto inicial',  font=('Arial', 10, 'bold'), text_color = 'black')],
+            [sg.Text('x¹:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='ponto_inicial', size=(10,1))],
+            [sg.Text('ε = ',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='epsilon', size=(10,1))],
+            [sg.Text('         ')],
+            [sg.Button('Calcular', size=(15,1)), sg.Button('Sair', size=(15,1))],
+            [sg.Text('         ')],
+            [sg.Text('         ')],
+            [sg.Text(size=(40,1), key='respostaDavidon1',  font=('Arial', 11, 'bold'))],
+            [sg.Text(size=(40,1), key='respostaDavidon2', text_color = 'black')],
+            [sg.Text(size=(40,1), key='respostaDavidon3', text_color = 'black')]
+            ]
+    return sg.Window('Davidon-Fletcher-Powell', layout, size=(400, 400), finalize=True, resizable=True)
+
+def window_newton():
+    layout = [[sg.Text('NEWTON', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text('         ')],
+            [sg.Text('Insira a função:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='expressao', size=(30,1))],
+            [sg.Text('Exemplo: x¹ = (0, 0) para R² o ponto inicial',  font=('Arial', 10, 'bold'), text_color = 'black')],
+            [sg.Text('x¹:',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='ponto_inicial', size=(10,1))],
+            [sg.Text('ε = ',  font=('Arial', 10, 'bold'), text_color = 'black'), sg.Input(key='epsilon', size=(10,1))],
+            [sg.Text('         ')],
+            [sg.Button('Calcular', size=(15,1)), sg.Button('Sair', size=(15,1))],
+            [sg.Text('         ')],
+            [sg.Text('         ')],
+            [sg.Text(size=(40,1), key='respostaNewton1',  font=('Arial', 11, 'bold'))],
+            [sg.Text(size=(40,1), key='respostaNewton2', text_color = 'black')],
+            [sg.Text(size=(40,1), key='respostaNewton3', text_color = 'black')]
+            ]
+    return sg.Window('Newton', layout, size=(400, 400), finalize=True, resizable=True)
+
+# Janela Inicial Principal
+def main_window():
+    layout = [[sg.Text(' ', size = (10, 1))],
+            [sg.Text('Selecione a rotina que deseja utilizar:', justification='center', font=('Arial', 11, 'bold'), text_color = '#921224')],
+            [sg.Text(' ', size = (4, 1))],
+            [sg.Button('Hooke & Jeeves', size=(20,1))],
+            [sg.Button('Newton', size=(20,1))],
+            [sg.Button('Gradiente', size=(20,1))],
+            [sg.Button('Fletcher & Reeves', size=(20,1))],
+            [sg.Button('Davidon-Fletcher-Powell', size=(20,1))],
+            [sg.Text(' ', size = (4, 1))],
+            [sg.Button('Sair')]]
+    return sg.Window('PO II - Trabalho 2', layout, size=(400, 400), element_justification='center', finalize=True, resizable=True)
+
+
+### Funções ###
+# def HookeJeeves ():
+# def Newton():
+# def Gradiente ():
+# def FletcherPowell():
+# def DavidonFletcherPowell():
+
+#Função para busca na reta (Newton):
+def MetodoNewton(funcao, a, b, epsilon):
+    x, y, z = symbols('x y z')
+    init_printing(use_unicode=True)
+
+    x = a
+    deriv1 = str(diff(funcao))
+    d1 = float(parser.parse(deriv1).evaluate({'x' : x}))    
+    k = []
+    k.append(x)
+    l = 0
+    
+    while abs(float(d1)) >= epsilon:
+    
+        deriv2 = str(diff(deriv1))
+        d2 = float(parser.parse(deriv2).evaluate({'x' : x}))
+        
+        if d2 != 0.0:
+            x = k[len(k)-1] - (d1 / d2)
+            k.append(x)
+            if((abs(k[len(k)-1] - k[len(k)-2]) / max(1, abs(k[len(k)-2]))) < epsilon):         
+                break
+            else:
+                d1 = float(parser.parse(deriv1).evaluate({'x' : x}))
+        else:
+            break
+        l = l + 1
+    return (k[len(k)-1], l)
+
+window1, window2, window3, window4, window5, window6, window7 = main_window(), None, None, None, None, None, None
+parser = Parser()
+
+while True:
+    window, event, valores = sg.read_all_windows()
+    if event == 'Sair'or event == sg.WIN_CLOSED:
+        window.close()
+
+    if window== window1 and event == 'Hooke & Jeeves':
+        window2 = window_hookeJeeves()
+        window2.move(window1.current_location()[0] + 50, window1.current_location()[1] + 50)
+
+    if window== window1 and event == 'Newton':
+        window3 = window_newton()
+        window3.move(window1.current_location()[0] + 50, window1.current_location()[1] + 50)
+
+    if window== window1 and event == 'Gradiente':
+        window4 = window_gradiente()
+        window4.move(window1.current_location()[0] + 50, window1.current_location()[1] + 50)
+
+    if window== window1 and event == 'Fletcher & Reeves':
+        window5 = window_fletcherReeves()
+        window5.move(window1.current_location()[0] + 50, window1.current_location()[1] + 50)
+
+    if window== window1 and event == 'Davidon-Fletcher-Powell':
+        window6 = window_davidonFletcherPowell()
+        window6.move(window1.current_location()[0] + 50, window1.current_location()[1] + 50)
+
+    if window == window2 and event == 'Calcular':
+       print ("Hooke & Jeeves"); 
+    
+    if window == window3 and event == 'Calcular':
+        print ("Newton"); 
+
+    if window == window4 and event == 'Calcular':
+        print ("Gradiente"); 
+
+    if window == window5 and event == 'Calcular':
+        print ("Fletcher & Reeves"); 
+    
+    if window == window6 and event == 'Calcular':
+        print ("Davidon-Fletcher-Powell"); 
